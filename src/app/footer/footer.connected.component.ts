b25335a37b738fb0da64722cb03e146d61893b79
Alex Imenwo <aleximenwo@gmail.com>
2024-09-01 13:29:40 +0100

    added username to email sub

--- src/app/footer/footer.connected.component.ts
+++ src/app/footer/footer.connected.component.ts
@@ -12,6 +12,7 @@ import Swal from 'sweetalert2';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { CommonModule } from '@angular/common';
 import {MatSelectModule} from '@angular/material/select';
+import { Platform } from '@angular/cdk/platform';
 
 @Component({
   selector: 'async-footer-connected',
@@ -185,12 +186,37 @@ import {MatSelectModule} from '@angular/material/select';
 export class FooterConnectedComponent implements OnInit {
   subscribeForm!: FormGroup;
   isSpinning = false;
+  userDevice = '';
+  username: string = 'business';
 
-  constructor(private fb: FormBuilder,  private footerService: FooterService,) {}
+  constructor(
+    private fb: FormBuilder,  
+    private footerService: FooterService,
+    private platform: Platform
+  ) {
+    if (this.platform.ANDROID || this.platform.IOS) {
+      //console.log('User is using a mobile device.');
+      this.userDevice = 'mobile'
+    } else {
+      //console.log('User is using a desktop device.');
+      this.userDevice = 'desktop'
+    }
+
+    const storedUsername = localStorage.getItem('username');
+    // Retrieve the data from local storage
+    if (storedUsername) {
+      this.username = storedUsername;
+      //console.log('Retrieved data from local storage:', this.username);
+    } else {
+      //console.log('Data not found in local storage');
+    }
+  }
 
   ngOnInit(): void {
     this.subscribeForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]]
+      email: ['', [Validators.required, Validators.email]],
+      userDevice: this.userDevice,
+      username: this.username
     });
   }
 
